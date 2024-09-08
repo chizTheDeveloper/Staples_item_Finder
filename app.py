05213b5d3cec194972760a93011a95359667996a
chizTheDeveloper <chume554@gmail.com>
2024-09-08 19:02:23 -0400

    removed spacy

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ import sqlite3
 import re
 from groq import Groq
 import os
-import spacy
-
-# Load the spaCy model for normalization (you can install spaCy with 'pip install spacy' and download the model 'python -m spacy download en_core_web_sm')
-nlp = spacy.load("en_core_web_sm")
 
 # Set the API key from either the environment variable or Streamlit secrets
 api_key = os.getenv("GROQ_API_KEY") or st.secrets["groq"]["api_key"]
